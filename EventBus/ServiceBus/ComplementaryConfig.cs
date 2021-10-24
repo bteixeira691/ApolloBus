2811a967c4b0a058ada2f4b3e598f340b211250f
dakar <dakar@DESKTOP-JISJAU7>
2021-10-24 17:25:14 +0100

    fix validation

--- EventBus/ServiceBus/ComplementaryConfig.cs
+++ EventBus/ServiceBus/ComplementaryConfig.cs
@@ -7,7 +7,7 @@ namespace ApolloBus.ServiceBus
     public class ComplementaryConfig
     {
         public string QueueOrTopic { get; set; }
-        public bool IsQueue{ get; set; }
+        public bool IsQueue { get; set; }
         public string ConnectionString { get; set; }
         public string SubscriptionName { get; set; }
 
@@ -16,10 +16,11 @@ namespace ApolloBus.ServiceBus
             if (IsQueue && (string.IsNullOrWhiteSpace(QueueOrTopic) || string.IsNullOrEmpty(QueueOrTopic)))
                 return false;
 
-            if (!IsQueue && (string.IsNullOrWhiteSpace(QueueOrTopic)|| string.IsNullOrEmpty(QueueOrTopic)) || (string.IsNullOrWhiteSpace(SubscriptionName) || string.IsNullOrEmpty(QueueOrTopic)))
-                return false;
+            if (!IsQueue)
+                if ((string.IsNullOrWhiteSpace(QueueOrTopic) || string.IsNullOrEmpty(QueueOrTopic)) || (string.IsNullOrWhiteSpace(SubscriptionName) || string.IsNullOrEmpty(QueueOrTopic)))
+                    return false;
 
-                return true;
+            return true;
         }
-    }    
+    }
 }
