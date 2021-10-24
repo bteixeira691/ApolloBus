abc74ceb6636689e1263eb21af89fcb0d2e05b23
dakar <dakar@DESKTOP-JISJAU7>
2021-10-24 16:21:36 +0100

    add samples servicebus

---
+++ MicroserviceA/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using ApolloBus.InterfacesAbstraction;
+using MicroserviceA.Event;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroserviceA.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IApolloBus _apolloBus;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IApolloBus apolloBus)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+
+            var eventq = new EventFromMicroserviceA();
+
+            eventq.Name = "teste02";
+            _apolloBus.Publish(eventq);
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
