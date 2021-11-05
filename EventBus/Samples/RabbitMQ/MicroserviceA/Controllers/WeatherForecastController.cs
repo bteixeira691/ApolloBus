f2346cedba33dd551513e6f48589361f832bf56b
dakar <dakar@DESKTOP-JISJAU7>
2021-11-05 00:04:12 +0000

    fix samples

---
+++ EventBus/Samples/RabbitMQ/MicroserviceA/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using ApolloBus.InterfacesAbstraction;
+using MicroserviceA.Event;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroserviceA.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IApolloBus _ApolloBus;
+
+        public WeatherForecastController(IApolloBus ApolloBus)
+        {
+            _ApolloBus = ApolloBus;
+        }
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+
+            _ApolloBus.Publish(new EventFromMicroserviceA()
+            {
+                Message = Summaries[rng.Next(Summaries.Length)],
+                Name="test01"
+            });
+
+            _ApolloBus.PublishDelay(new EventFromMicroserviceA()
+            {
+                Message = Summaries[rng.Next(Summaries.Length)],
+                Name = "test02"
+            }, 2);
+
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
